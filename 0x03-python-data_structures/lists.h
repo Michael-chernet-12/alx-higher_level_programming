def1c30d76b8b309b343ad59a49cc4d9e074799a
Michael chernet <zemichaelchernet@gmail.com>
2022-08-29 01:08:15 -0700

    task 0-13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -38,7 +38,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 
 void free_listint(listint_t *head);
 
-
+void print_python_list_info(PyObject *p);
 
 int is_palindrome(listint_t **head);
 
