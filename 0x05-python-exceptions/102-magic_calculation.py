9d7c7606fbe5ee1d0f836a0d061d1ccf81125efe
Michael chernet <zemichaelchernet@gmail.com>
2022-09-12 12:11:25 -0700

    task 100-103

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 3):
+    for n in range(1, 3):
         try:
-            if i > a:
+            if n > a:
                 raise Exception('Too far')
             else:
-                result += (a ** b) / 
-        except:
-            result = b + a
+                result += a ** b / n
+        except Exception:
+            result = a + b
             break
-        return result
+        return (result)
