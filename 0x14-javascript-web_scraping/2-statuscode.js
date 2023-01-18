85ec2ceb1d889477138426bc283d5e2e87af42f5
Michael chernet <zemichaelchernet@gmail.com>
2023-01-18 08:35:45 -0800

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const axios = require('axios');
-const args = process.argv.slice(process.argv.length - 1);
-
-axios({
-  method: 'GET',
-  url: args[0]
-}).then(res => {
-  console.log('code: '.concat(res.status));
-}).catch(err => console.log('code: '.concat(err.response.status)));
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
