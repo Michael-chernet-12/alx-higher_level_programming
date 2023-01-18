113c1df485fd4ae7c5a868cbb1b254581b8faa40
Michael chernet <zemichaelchernet@gmail.com>
2023-01-18 08:53:18 -0800

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-const ID = process.argv[2];
-const endPoint = `https://swapi-api.hbtn.io/api/films/${ID}`;
 
-axios.get(endPoint)
-  .then(res => console.log(res.data.title))
-  .catch(err => console.log(err.message));
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
